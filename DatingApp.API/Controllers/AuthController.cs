d85e3c0910e164772e0c8450fb2f4da1a00434f8
Ernesto A <ernesto.acosta1086@gmail.com>
2019-10-22 15:20:20 -0400

    Using DTOs (Data Transfer Object)

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,21 +18,21 @@ namespace DatingApp.API.Controllers
         }
 
         [HttpPost ("register")]
-        public async Task<IActionResult> Register (string username, string password)
+        public async Task<IActionResult> Register (UserForRegisterDto userForRegisterDto)
         {
             //validate request
 
-            username = username.ToLower();
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if(await _repo.UserExist(username))
-                return BadRequest("Usernam already exist");
+            if(await _repo.UserExist(userForRegisterDto.Username))
+                return BadRequest("Username already exist");
 
             var userToCreate = new User
             {
-                Username = username
+                Username = userForRegisterDto.Username
             };
 
-            var userCreated = await _repo.Register(userToCreate, password);
+            var userCreated = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
         }
