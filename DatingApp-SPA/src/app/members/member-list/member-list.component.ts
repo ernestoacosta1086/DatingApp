5dc3698622590701ace8201ac15ac660ed3a77d1
Ernesto A <ernesto.acosta1086@gmail.com>
2019-11-18 16:01:04 -0500

    Creating Member Cards to display on our Member list page

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../_models/user';
-import { UserService } from '../_services/user.service';
-import { AlertifyService } from '../register/alertify.service';
+import { User } from '../../_models/user';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../register/alertify.service';
 
 @Component({
   selector: 'app-member-list',
